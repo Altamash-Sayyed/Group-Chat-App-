00af6bb2faa21b17eaa3dd320a0a79d8f0fddf54
Altamash <altamashsayyed023@gmail.com>
2022-04-24 23:30:39 +0530

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 http.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
