e22638a2f62703fd7004d17cf3c2a39f78b6d0b7
Altamash <altamashsayyed023@gmail.com>
2022-04-24 23:15:20 +0530

    make it better and easy

---
+++ public/client.js
@@ -0,0 +1,51 @@
+const socket = io()
+let nam;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+do { 
+    nam = prompt('Please enter your name: ')
+} while(!nam)
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    // Append 
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    // Send to server 
+    socket.emit('message', msg)
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
